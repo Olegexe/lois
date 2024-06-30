f597fdc62ec5b51c6d51f1cc55447e3312f9ad0b
Olegexe <43041704+Olegexe@users.noreply.github.com>
2024-06-30 10:03:56 +0300

    old-design

--- webpack.config.js
+++ webpack.config.js
@@ -102,7 +102,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'assets/img/[name][ext][query]'
